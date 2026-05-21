4e5408b7d065ea2036d9cbc281ee1860303a2c41
ul1 <ulwan0911@gmail.com>
2026-05-21 19:54:26 +0700

    newScript

--- Assets/script.cs
+++ Assets/script.cs
@@ -1,16 +1,47 @@
 using UnityEngine;
+using UnityEngine.XR;
 
-public class script : MonoBehaviour
+public class Whiteboard : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private XRGrabInteractable currentInteractable;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.TryGetComponent<XRGrabInteractable>(out XRGrabInteractable interactable))
+            currentInteractable = interactable;
+
+        // if (interactable != null)
+        // {
+        //     currentInteractable = interactable;
+        //     currentInteractable.selectExited.AddListener(OnObjectReleased);
+        // }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerStay(Collider other)
     {
-        
+        if (currentInteractable == null)
+            return;
+
+        if (!currentInteractable.isSelected)
+        {
+            currentInteractable.transform.SetParent(this.transform);
+            currentInteractable.TryGetComponent<Rigidbody>().enabled = false;
+        }
+
+        // if (currentInteractable != null && other.GetComponentInParent<XRGrabInteractable>() == currentInteractable)
+        // {
+        //     currentInteractable.selectExited.RemoveListener(OnObjectReleased);
+        //     currentInteractable = null;
+        // }
     }
+
+    // private void OnObjectReleased(SelectExitEventArgs args)
+    // {
+    //     Debug.Log("Object released inside the trigger box!");
+    //
+    //     // Execute your placement logic here
+    //
+    //     // currentInteractable.selectExited.RemoveListener(OnObjectReleased);
+    //     // currentInteractable = null;
+    // }
 }
